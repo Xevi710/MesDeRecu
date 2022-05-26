b8346ba5d5e661f08f9665a443b8fae320a03d3f
Xevi710 <fcolpel930@g.educaand.es>
2022-05-26 20:20:53 +0200

    commit

---
+++ dwec/segundo.js
@@ -0,0 +1,24 @@
+function mensaje(){
+    alert("mensaje");
+}
+window.addEventListener('DOMContentLoaded', () => {
+    
+    document.querySelector("button").addEventListener("click",mensaje);
+    document.querySelector("button:nth-child(2)").addEventListener("click",(evt)=>{
+        //alert(evt.type);
+        const nombre ="Susana";
+
+        evt.target.textContent = 'Hola';
+        document.querySelector("div").innerHTML="<span>Texto generado desde JS</span>"
+
+    });
+
+    document.querySelector('button:nth-of-type(3)').addEventListener("click",(evt)=>{
+        evt.target.style.backgroundColor="yellow";
+        evt.target.style.width="20vw";
+    });
+
+    document.querySelector("button:last-of-type").addEventListener("click",(evt)=>{
+        evt.target.classList.toggle('estilos');
+    });
+});
