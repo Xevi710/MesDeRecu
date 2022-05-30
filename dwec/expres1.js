9ca825f2d08ae21a4cc8a282118ddb46d682cb19
Xevi710 <fcolpel930@g.educaand.es>
2022-05-30 19:37:37 +0200

    commit

---
+++ dwec/expres1.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoader",()=>{
+    let eles = document.querySelector('#eles').value;
+    
+    var mipatron = /^[aeiou]{2,5}$/i;
+    console.log(mipatron.test(eles.value))
+
+    if (mipatron.test(eles)) {
+        document.querySelector('#eles ~ span').innerText="OK";
+        document.querySelector('#eles ~ span').classList.add("error")
+    }else{
+        document.querySelector('#eles ~ span').classList.remove("noerror");
+    }
+
+    document.querySelector('#enviar2').addEventListener("click", ()=>{
+        console.log(document.querySelector('#cod').checkValidity());
+    });
+});
